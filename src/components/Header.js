06d1f788e5ec393196fdab7ab2ad1ff5d20cbc9d
mh-saeed <mh.saeed.aq@gmail.com>
2023-06-21 00:28:10 +0500

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header style={styles.header}>
-      <h1 style={styles.headerText}>MH-Saeed’s Drive</h1>
+      <h1 style={styles.headerText}>MS’s Drive</h1>
       <p style={styles.headerSubText}>A File Manager created by mh-saeed</p>
     </header>
   );
