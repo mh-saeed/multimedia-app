45c1abd865f4f27097e3552ecc638b5ff6eb38f5
mh-saeed <mh.saeed.aq@gmail.com>
2023-06-21 01:04:46 +0500

    share added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ImageViewer } from "./components/ImageViewer";
 import { DocumentViewer } from "./components/DocumentViewer";
 import { AudioPlayer } from "./components/AudioPlayer";
 import { VideoPlayer } from "./components/VideoPlayer";
+import { ShareButton } from "./components/ShareButton";
 
 import { Pie, Bar } from "react-chartjs-2";
 import {
@@ -190,6 +191,13 @@ export default function App() {
             >
               Delete
             </button>
+            <ShareButton
+              styles={styles}
+              selectedFile={selectedFile}
+              myFiles={myFiles}
+              setMyFiles={setMyFiles}
+              setSelectedFile={setSelectedFile}
+            />
           </div>
           <div style={styles.fileContainer}>
             <div style={{ width: "100%", padding: 10 }}>
@@ -341,5 +349,46 @@ const styles = {
     cursor: "pointer",
     fontWeight: "bold",
     backgroundColor: "#eee",
+  },
+  modalOverlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "rgba(0, 0, 0, 0.5)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 9999,
+  },
+  shareModal: {
+    background: "#fff",
+    padding: "20px",
+    borderRadius: "4px",
+    maxWidth: "400px",
+  },
+  shareOptionsList: {
+    listStyle: "none",
+    padding: 0,
+    margin: "10px 0",
+  },
+  shareOption: {
+    cursor: "pointer",
+    padding: "5px",
+    margin: "5px 0",
+    borderRadius: "4px",
+    background: "#f5f5f5",
+  },
+  sharecloseButton: {
+    background: "#f5f5f5",
+    border: "none",
+    padding: "8px 12px",
+    borderRadius: "4px",
+    cursor: "pointer",
+  },
+  error: {
+    color: "red",
+    marginTop: "10px",
   },
 };
---
+++ src/components/ShareButton.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+export const ShareButton = ({
+  styles,
+  selectedFile,
+  myFiles,
+  setMyFiles,
+  setSelectedFile,
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPlatform, setSelectedPlatform] = useState("");
+  const [error, setError] = useState("");
+
+  const handleShareButtonClick = () => {
+    if (selectedFile) {
+      setShowModal(true);
+      setError("");
+    } else {
+      // Show tooltip or error message indicating that no item is selected
+      setError("Please select an item to share.");
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleShareOptionClick = (platform) => {
+    if (selectedFile) {
+      setSelectedPlatform(platform);
+      shareFile(platform);
+      setShowModal(false);
+    }
+  };
+
+  const handleDeleteFile = () => {
+    if (selectedFile) {
+      const newFiles = myFiles.filter((file) => file.id !== selectedFile.id);
+      setMyFiles(newFiles);
+      setSelectedFile(null);
+    }
+  };
+
+  const shareFile = (platform) => {
+    // Share the file on the selected platform
+    const shareUrl = `https://mh-saeed-multimedia-app.netlify.app/files/${selectedFile.path}`;
+    const shareText = `Check out this file: ${selectedFile.name}`;
+    const encodedShareUrl = encodeURIComponent(shareUrl);
+    const encodedShareText = encodeURIComponent(shareText);
+
+    let shareLink = "";
+
+    switch (platform) {
+      case "Facebook":
+        shareLink = `https://www.facebook.com/sharer/sharer.php?u=${encodedShareUrl}`;
+        break;
+      case "Twitter":
+        shareLink = `https://twitter.com/share?url=${encodedShareUrl}&text=${encodedShareText}`;
+        break;
+      case "LinkedIn":
+        shareLink = `https://www.linkedin.com/shareArticle?url=${encodedShareUrl}&title=${encodedShareText}`;
+        break;
+      default:
+        setError("Invalid platform");
+        return;
+    }
+
+    const newWindow = window.open(shareLink, "_blank");
+
+    if (
+      !newWindow ||
+      newWindow.closed ||
+      typeof newWindow.closed === "undefined"
+    ) {
+      setError(
+        "Pop-up blocked by the browser. Please enable pop-ups and try again."
+      );
+    }
+  };
+
+  const shareOptions = ["Facebook", "Twitter", "LinkedIn"];
+
+  return (
+    <div>
+      <button style={styles.controlButton} onClick={handleShareButtonClick}>
+        Share
+      </button>
+      {showModal && (
+        <div style={styles.modalOverlay}>
+          <div style={styles.shareModal}>
+            <h3>Select a platform to share:</h3>
+            {error && <p style={styles.error}>{error}</p>}
+            <ul style={styles.shareOptionsList}>
+              {shareOptions.map((platform) => (
+                <li
+                  key={platform}
+                  onClick={() => handleShareOptionClick(platform)}
+                  style={styles.shareOption}
+                >
+                  {platform}
+                </li>
+              ))}
+            </ul>
+            <button style={styles.sharecloseButton} onClick={handleCloseModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
